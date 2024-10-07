faaa50b87f0a420c3dba7fa3d08df2624a0165ea
YPetremann <y.petremann@gmail.com>
2024-10-07 18:13:14 +0200

    feat: improve Vite configuration for development and production modes

--- vite.config.ts
+++ vite.config.ts
@@ -5,15 +5,16 @@ import iconify from "@tomjs/vite-plugin-iconify";
 import { VitePWA } from "vite-plugin-pwa";
 import { minimal2023Preset as preset } from "@vite-pwa/assets-generator/config";
 import { ViteMinifyPlugin } from "vite-plugin-minify";
-import mkcert from'vite-plugin-mkcert'
 import fs from "fs";
 // https://vitejs.dev/config/
 export default defineConfig(async ({ mode, command,...args }) => {
+  const isDev=mode==="development"
+  const isProd=mode==="production"
   return {
     base: "/MTR-Explorer/",
     server: { host: true },
     preview: { 
-      https:{
+      https:isDev &&{
         cert: fs.readFileSync("certs/cert.pem"),
         key: fs.readFileSync("certs/dev.pem"),
       }, 
@@ -53,13 +54,13 @@ export default defineConfig(async ({ mode, command,...args }) => {
         },
       }),
       ViteMinifyPlugin({}),
-      command === "build" && mkcert({
-        savePath: './certs',
-        autoUpgrade: true,
-        hosts: ['localhost'],
-        force: true,
-      }),
-      command !== "build" &&
+      isDev && [
+        (await import("vite-plugin-mkcert")).default({
+          savePath: './certs',
+          autoUpgrade: true,
+          hosts: ['localhost'],
+          force: true,
+        }),
         (await import("vite-plugin-checker")).default({
           typescript: true,
           eslint: { lintCommand: 'eslint "./src/**/*.{js,jsx,ts,tsx}"', useFlatConfig: true },
@@ -68,6 +69,7 @@ export default defineConfig(async ({ mode, command,...args }) => {
           overlay:{ initialIsOpen: "error" },
           terminal:false,
         }),
+      ]
     ],
     worker: { plugins: () => [comlink()] },
     css: { preprocessorOptions: { scss: { silenceDeprecations: ["legacy-js-api"] } } },
